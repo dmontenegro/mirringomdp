a49ca51aa35fc04c72d63d748a741f802b5892ee
daniel <daniel@debian.(none)>
2012-10-31 23:41:42 -0500

    Se hizo la funcion construir generica, la receta solo contiene las acciones propias, descomprimir y demas ya se encuentran en construir. Se descubrio que trabajando con el .mdp se presentan problemas al comprimirlo por lo tanto maneje todo con .tar.gz

--- construir.sh
+++ construir.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
+. ./$1
 
-source $1
+name=`grep "pkgname=" $1`
+name=${name#*=}
+ver=`grep "pkgver=" $1`
+ver=${ver#*=}
+rel=`grep "pkgrel=" $1`
+rel=${rel#*=}
+owner=`grep "pkgowner=" $1`
+owner=${owner#*=}
+url=`grep "url=" $1`
+url=${url#*=}
+sourc=`grep "source=" $1`
+sourc=${sourc#*=}
+shasum=`grep "sha512sums=" $1`
+shasum=${shasum#*=}
+
+path=$PWD
+wget $source
+ss=`sha512sum $name-$ver.tar.gz| cut -d " " -f 1`
+echo $shasum
+echo $ss
+if [ $ss != $shasum ]
+then
+    rm $name-$ver.tar.gz
+    echo "Suma criptografica mala."
+exit 1
+fi
+tar xvfz $name-$ver.tar.gz
+mkdir paquete
+cp -r $name-$ver/* paquete
+cd paquete
 build
+tar cvfz $name-$ver-$rel.tar.gz *
+mv $name-$ver-$rel.tar.gz $path
+cd $path 
+rm -rf paquete
+rm -rf $name-$ver
+rm $name-$ver.tar.gz
--- receta.sh
+++ receta.sh
@@ -9,31 +9,11 @@ url="http://www.gecode.org"
 license='MIT'
 depends=''
 source="http://escher.puj.edu.co/~farhat/helloworld-0.1.tar.gz"
-sha512sums='ce60395855714433442cbef5344bf167cbac48100d0033fbfb641f858925159cd0176ced715cfbead4a36b62a79358f782aa0748ac7925dd062c87a6ec93a83c'
+sha512sums=ce60395855714433442cbef5344bf167cbac48100d0033fbfb641f858925159cd0176ced715cfbead4a36b62a79358f782aa0748ac7925dd062c87a6ec93a83c
 
 build() {
-    path=$PWD
-    wget $source
-    ss=`sha512sum $pkgname-$pkgver.tar.gz| cut -d " " -f 1`
-    echo $ss
-	if [ $ss != $sha512sums ]
-    then
-	rm $pkgname-$pkgver.tar.gz 
-	echo "Suma criptografica mala."
-	exit 1
-    fi    
-    tar xvfz $pkgname-$pkgver.tar.gz
-    cd $pkgname-$pkgver
-    chmod +x helloworld
-    mkdir paquete
-    cp helloworld paquete/
-    
-  
-    tar cvfz $pkgname-$pkgver-$pkgrel.mdp paquete
-    mv $pkgname-$pkgver-$pkgrel.mdp $path
-    cd $path
-    rm $pkgname-$pkgver.tar.gz
-    rm -rf $pkgname-$pkgver
+    chmod +x $pkgname
+    touch hola.txt
 }
 
 
