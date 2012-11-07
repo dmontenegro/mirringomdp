21baf0d2dfcfa33d79750df29f230610dab2a180
Alejandro Meneses <alejo@CoquenII.(none)>
2012-11-06 20:42:35 -0500

    Agregado creación de directorios y copia de sus respectivos archivos

--- install.sh
+++ install.sh
@@ -1,24 +1,51 @@
 #!/bin/sh
+
+#Here's where all dependencies are listed
 depends='atool html2text'
 
-confpath=/etc/mirringo/config
-confFiles='buscar.sh construir.sh info.sh new.sh receta.sh test.sh'
+#Defining the path for the files of the program
+#and the list of files that are going to be copied.
+confpath=/etc/mirringo
+confFiles="buscar.sh construir.sh info.sh new.sh receta.sh info.txt"
 
-bdpath=/var/mirringo/
-bdFiles='
+bdpath=/var/mirringo
+bdFiles='mybd.bd'
 
-binpath=/usr/bin/
+binpath=/usr/bin
+binFile='mirringo.sh'
 
 
 #This is going to be used as a partial solution
 #it only works on debian-based linux distros
 
 if [ $(whoami) != "root" ] ; then
-	echo 'sorry, you must be root to run this script' > &2
+	echo 'sorry, you must be root to run this script'
       exit 1
 else
+	
 	echo 'Installing dependencies...'
 	apt-get install depends
+	
+	#Making the paths for the program installation
+	mkdir $confpath
+	mkdir $confpath/config 	
+	mkdir $binpath
+		
+	#Copying the configuration files to the corresponding directory
 	echo 'Installing program...'
+	for instFile in $confFiles
+		do
+			cp $instFile $confpath/config
+		done
+	#Copying the database files to the corresponding directory	
+	for instFile in $bdFiles
+		do
+			cp $instFile $bdpath
+		done
+	
+	#Copying the executable file to the corresponding directory
+	cp $binFile $binpath
 	
 fi
+
+echo "done :D"
