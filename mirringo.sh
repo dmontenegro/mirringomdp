63d00f97ba35eaff417cfc3fecaeaa15d8a54bae
Juan Pablo Giron <jpgiron@javeriana.edu.co>
2012-11-06 23:20:18 -0500

    Se corrigio los condicionales de la funcion Mirringo

--- mirringo.sh
+++ mirringo.sh
@@ -4,35 +4,37 @@
 #Este Script representa el Main del Manejador de Paquetes
 
 
-function Mirringo()
+function Mirringo
 {
-   if [ $1 == "--install" || $1 == "-i" ] then
-	
+
+   if  [[ $1 = "--install" || $1 = "-i" ]]  ; then
+	echo "installing"
 	source instalar.sh $2
    fi
 
-   if [ $1 == "--unistall" || $1 == "-u" ] then
-	
+   if [[ $1 = "--unistall" || $1 = "-u" ]]; then
+	echo "unistalling"
 	source eliminar.sh $2
    fi
 
-   if [ $1 == "--build" || $1 == "-b" ] then
-        
+   if [[ $1 = "--build" || $1 = "-b" ]] ; then
+        echo "building"
 	source construir.sh $2
    fi
 
-   if [ $1 == "--update" || $1 == "-up"] then
-
+   if [[ $1 = "--update" || $1 = "-up" ]] ; then
+	echo "Update"
 	source actualizar.sh $2
    fi
 
-   if [ $1 == "--version" || $1 == "-v"] then
-	
+   if [[ $1 = "--version" || $1 = "-v" ]] ; then
+	echo "version"
 	source versionar.sh $2
    fi
 
-   if [ $1 == "--search" || $1 == "-s" ] then
-
+   if [[ $1 = "--search" || $1 = "-s" ]] ; then
+	echo "search"
 	source buscar.sh
    fi 
 }
+Mirringo $1 $2
