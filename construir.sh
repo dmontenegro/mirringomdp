d34193cad828cb81881fb5cbf645cfc5e7f1b624
Daniel Montenegro <dmontenegro@localhost.localdomain>
2012-11-08 15:39:51 -0500

    se agrego a la funcion construir la adicion a la base de datos. falta que sea funcion construir

--- construir.sh
+++ construir.sh
@@ -8,6 +8,9 @@
 
 #Construir.sh depende de: atool. libreria la cual tiene apack, aunpack y als.
 #para instalar atool es apt-get install atool
+
+#function construir{
+
 pkgdir=paquete
 srcdir=$pkgname-$pkgver
 path=$PWD
@@ -20,11 +23,9 @@ wget $source
 
 ss=`sha512sum $pkgname-$pkgver.* | cut -d " " -f 1`
 
-#echo $sha512sums
-#echo $ss
 if [ $ss != $sha512sums ]
 then
-    rm $pkgname-$pkgver.tar.bz2
+    rm $pkgname-$pkgver.*
     echo "Suma criptografica mala."
 exit 1
 fi
@@ -34,15 +35,23 @@ aunpack $pkgname-$pkgver.*
 cd Unpack-* #Entra al paquete descomprimido
 cp -r $pkgname-$pkgver* $path/
 cd /$path
-rm -rf Unpack-*
+rm -rf Unpack-* #copia afuera la carpeta pkgname-pkver 
 
 build
 cd $pkgdir
 tar cvfz $pkgname-$pkgver-$pkgrel.tar.gz *  
 mv $pkgname-$pkgver-$pkgrel.tar.gz $path
 cd $path 
+
+
+asd=$pkgname"\"","\""v$pkgver"\"","\""v$pkgrel"\"","\""$pkgdesc"\"","\""$url"\"","\""$license"\"","\""$depends"\"","\""$source"\"","\""$sha512sums 
+
+echo $asd >>mybd.bd
+
 rm -rf $pkgdir
 rm -rf $srcdir
 rm -rf $pkgname-$pkgver
 #rm $pkgname-$pkgver.tar.bz2
 rm $pkgname-$pkgver.*
+
+#}
