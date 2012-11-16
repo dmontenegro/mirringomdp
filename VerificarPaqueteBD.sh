dc6df2e1d07f7000fccdbb3acc48c3bdba5d5e94
Juan Pablo Giron Ruiz <jpgiron@javerianacali.edu.co>
2012-11-16 01:34:51 -0500

    Se comenta para que sirve el numero 3 como valor de retorno

--- VerificarPaqueteBD.sh
+++ VerificarPaqueteBD.sh
@@ -3,16 +3,15 @@
 #Retorna 0 cuando el paquete no se encuentra en la BD
 #Retorna 1 cuando hay una version mas vieja del paquete en la BD
 #Retorna 2 cuando hay una version mas nueva del paquete en la BD
-#Retorna 3 cuando el paquete que se va a instalar ya esta en la BD
-
+#Retorna 3 cuando el paquete a instlar es exactamente igual a un paquete ya instalado en el sistema 
 #$1 representa el nombre del paquete a instalar
 #$2 representa la version de la receta
 #$3 representa la version del paquete
 
 
-PKGNAME=$(grep $1 mybd.bd|wc -l)
-
-if [ $PKGNAME -eq 0 ]
+SearchNAME=$(grep $1 mybd.bd|wc -l)
+PKGNAME=$1
+if [ $SearchNAME -eq 0 ]
 then
 	echo 0
 else
