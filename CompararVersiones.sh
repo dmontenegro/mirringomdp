994a364c889ab64c39ae016e74659abc518b6a58
Juan Pablo Giron Ruiz <jpgiron@javerianacali.edu.co>
2012-11-16 16:05:43 -0500

    Se cambio el Comparar en cuento a comprar si las versiones son las mismas

--- CompararVersiones.sh
+++ CompararVersiones.sh
@@ -4,29 +4,32 @@
 #$2 representa la version que se va a comparar
 #$3 representa la columna o version que se quiere comparar
 
-for i in $(seq 1 $1)
-do
-	#Num Corresponde a la version del paquete que
-	#se desea instalar
-	Num=$(echo $2|tr [="."=] ' '|cut -d " " -f$i)
-	
-	#Num2 Corresponde a la version del paquete instalado
-	Num2=$(grep $PKGNAME /tmp/mirringo/Instalados/Instalados.bd |cut -d "," -f$3 |tr [="."=] ' ' | cut -d " " -f$i)
-	
-	if [ $Num -gt $Num2  ]
-	then
-		echo 2
-		break
-	elif [ $Num -lt $Num2  ]
-	then
-		echo 1
-		break
-	
-	elif [ $Num -eq $Num2 ]
-	then
-		echo 3
-		break
-	fi	
-done
+Var=$(grep $PKGNAME mybd.bd|cut -d "," -f$3)
+
+if [ "$2" = "$Var" ]
+then
+	echo 3
+
+else
+
+	for i in $(seq 1 $1)
+	do
+		#Num Corresponde a la version del paquete que se desea instalar
+		Num=$(echo $2|tr [="."=] ' '|cut -d " " -f$i)
+		
+		#Num2 Corresponde a la version del paquete instalado
+		Num2=$(grep $PKGNAME mybd.bd|cut -d "," -f$3 |tr [="."=] ' ' | cut -d " " -f$i)
+		
+		if [ $Num -gt $Num2  ]
+		then
+			echo 2
+			break
+		elif [ $Num -lt $Num2  ]
+		then
+			echo 1
+			break
+		fi
+	done
+fi
 
 
