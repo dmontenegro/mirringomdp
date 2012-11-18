2b82c410b25c7aa61f3e354e7618339e96580208
Juan Pablo Giron Ruiz <jpgiron@javerianacali.edu.co>
2012-11-18 16:24:04 -0500

    Se cambio cut por awk para solucionar el problema del separador

--- CompararVersiones.sh
+++ CompararVersiones.sh
@@ -4,7 +4,20 @@
 #$2 representa la version que se va a comparar
 #$3 representa la columna o version que se quiere comparar
 
-Var=$(grep $PKGNAME /var/mirringo/mybd.bd |cut -d '","' -f$3)
+#Var=$(grep $PKGNAME /var/mirringo/mybd.bd |cut -d '","' -f$3)
+
+Col=$3  
+
+if [ Col -eq 2 ]
+then
+
+	Var=$(grep $PKGNAME /var/mirringo/mybd.bd| awk -F '","' '{print $2}')
+elif [ Col -eq 3 ]
+then
+	
+	Var=$(grep $PKGNAME /var/mirringo/mybd.bd| awk -F '","' '{print $3}')
+fi
+
 
 if [ "$2" = "$Var" ]
 then
@@ -18,8 +31,8 @@ else
 		Num=$(echo $2|tr [="."=] ' '|cut -d " " -f$i)
 		
 		#Num2 Corresponde a la version del paquete instalado
-		Num2=$(grep $PKGNAME /var/mirringo/mybd.bd |cut -d '","' -f$3 |tr [="."=] ' ' | cut -d " " -f$i)
-		
+		#Num2=$(grep $PKGNAME /var/mirringo/mybd.bd |cut -d '","' -f$3 |tr [="."=] ' ' | cut -d " " -f$i)
+		Num2=$(echo $Var|tr [="."=] ' ' | cut -d " " -f$i)
 		if [ $Num -gt $Num2  ]
 		then
 			echo 2
