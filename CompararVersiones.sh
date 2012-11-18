417d90ea208ecdeed995b693a6c0f0eaf550f60e
Juan Pablo Giron Ruiz <jpgiron@javerianacali.edu.co>
2012-11-18 16:34:49 -0500

    Se organizo mejor el codigo

--- CompararVersiones.sh
+++ CompararVersiones.sh
@@ -30,7 +30,6 @@ else
 		Num=$(echo $2|tr [="."=] ' '|cut -d " " -f$i)
 		
 		#Num2 Corresponde a la version del paquete instalado
-		#Num2=$(grep $PKGNAME /var/mirringo/mybd.bd |cut -d '","' -f$3 |tr [="."=] ' ' | cut -d " " -f$i)
 		Num2=$(echo $Var|tr [="."=] ' ' | cut -d " " -f$i)
 		if [ $Num -gt $Num2  ]
 		then
